1419b71f1f9910e8e5ec3e47a4e6615ff33ddeb5
GaoHZ <gaohaizhao@fenrir-tec.com>
2026-03-18 14:03:48 +0800

    create swiftUI project

---
+++ schedule-notes/schedule_notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  schedule_notesApp.swift
+//  schedule-notes
+//
+//  Created by gaohaizhao on 2026/3/18.
+//
+
+import SwiftUI
+
+@main
+struct schedule_notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
